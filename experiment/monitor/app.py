6f4dbaeeef22ed9707266941448ba1ab1ade6cc8
jgaray <jgaray@corelogic.com>
2023-09-15 20:41:48 -0500

    Command-processor: send topic message

--- experiment/monitor/app.py
+++ experiment/monitor/app.py
@@ -1,10 +1,32 @@
 # This is a sample Python script.
+import json
 import logging
-
+import uuid
+from datetime import datetime
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 import requests
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    key_serializer=lambda k: json.dumps(k).encode('utf-8'),  # Serialize the key as JSON
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')  # Serialize the value as JSON
+)
+kafka_topic = 'monitor'
+
+def send_topic_message(ms, status):
+    monitor_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    message = {
+        "ms-name": ms,
+        "monitor-timestamp": monitor_timestamp,
+        "ms-status": status
+    }
+    key = str(uuid.uuid4())
+    producer.send(kafka_topic, key=key, value=message)
+    producer.flush()
+    logger.info(f'Published Monitor Topic for ms: {ms} with status: {status}')
 
 
 def call_api_endpoint():
@@ -20,10 +42,12 @@ def call_command():
     response = requests.get(url)
     if response.status_code == 200:
         logger.info("API call command successful")
+        send_topic_message("Command", "OK")
         # Process the API response data as needed
         # ...
     else:
         logger.info(f"Failed to call API. Status code: {response.status_code}")
+        send_topic_message("Command", "FAILED")
 
 
 def call_command_processor():
@@ -33,17 +57,19 @@ def call_command_processor():
     response = requests.get(url)
     if response.status_code == 200:
         logger.info("API call command processor successful")
+        send_topic_message("Command Processor", "OK")
         # Process the API response data as needed
         # ...
     else:
         logger.info(f"Failed to call API. Status code: {response.status_code}")
+        send_topic_message("Command Processor", "FAILED")
 
 
 logging.basicConfig(filename='monitorlogs.log',
                     filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
 
 logger = logging.getLogger('Monitor')
